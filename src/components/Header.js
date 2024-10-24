54f7a892a24b8818e73085872aba98ab446f9ce3
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-10-24 13:40:43 +0900

    ✨ ヘッダーの追加

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import classes from "./Header.module.css";
+
+export function Header() {
+    return (
+
+        <header className={classes.header}>
+            <Link href="/">
+                <div className={classes.anchor}>Index</div>
+            </Link>
+            <Link href="/about">
+                <div className={classes.anchor}>About</div>
+            </Link>
+
+        </header>
+    );
+}
