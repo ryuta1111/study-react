3d33dfce6e2a070ad8fe79cbc3b9a9d9acffdba5
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-10-25 20:20:28 +0900

    ♻️ Linksのリファクタリング

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,36 +1,41 @@
-import Image from "next/image";
+// import Image from "next/image";
 import classes from "./Links.module.css"
+
+const ITEMS = [
+    {
+        href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+        title: "Deploy now",
+        class: "rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5",
+        // image: {
+        //     class:"dark:invert",
+        //     src:"/vercel.svg",
+        //     alt:"Vercel logomark",
+        //     width:20,
+        //     height:20
+        // }
+    },
+    {
+        href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+        title: "Read our docs",
+        class: "rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44",
+    }
+]
 
 export function Links() {
     return (
-
         <div className={classes.grid}
         //  className="flex gap-4 items-center flex-col sm:flex-row"
         >
-            <a
-                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-            <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-            />
-            Deploy now
-            </a>
-            <a
-                className={classes.card}
-                // className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Read our docs
-            </a>
+            {ITEMS.map((item) => {
+                return (
+                    <div key={item.href}>
+                        <a  href={item.href} className={item.class}>
+                            <h3 className={classes.title}>🔸{item.title}</h3>
+                        </a>
+                        {/* <image className={item.image}/> */}
+                    </div>
+                );
+            })}
         </div>
     );
 }
