1dcf19d051ec3d8fb51793dd4484c1ced927673d
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-10-27 18:23:01 +0900

    ✨ useStateで配列を扱ってみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,6 +21,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if(count < 10){
@@ -39,6 +40,16 @@ export default function Home() {
     };
     setText(e.target.value.trim());
   },[])
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if(prevArray.some(item => item === text)){
+        alert("同じ要素がすでに存在します。");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  },[text])
 
   useEffect(() => {
     document.body.style.backgroundColor = "green";
@@ -66,6 +77,14 @@ export default function Home() {
         value={text}
         onChange={handleChange}
       />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return(
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
       <Main page="index"/>
 
       <Footer />
